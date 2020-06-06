bed6468d8169b0a54471955f0baaeab055036aad
Jose <DESKTOP-422P2HN>
2020-06-06 17:06:41 +0100

    Criação das Entidades

---
+++ QuickBuy.Dominio/Entity/Pedido.cs
@@ -0,0 +1,24 @@
+﻿using QuickBuy.Dominio.ValueObject;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace QuickBuy.Dominio.Entity
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsurarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade{ get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroCompleto { get; set; }
+        public int FormaPagamentoID { get; set; }
+        public FormaPagamento FormaPagamento{ get; set; }
+        public ICollection<ItemPedido> Pedidos { get; set; }
+
+    }
+}
---
+++ QuickBuy.Dominio/ValueObject/FormaPagamento.cs
@@ -0,0 +1,39 @@
+﻿using QuickBuy.Dominio.Enum;
+using System;
+
+namespace QuickBuy.Dominio.ValueObject
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public string Nome { get; set; }
+        public bool IsBoleto {
+            get
+            {
+                return Id == (int)TipoFormaPagamentoEnum.Boleto;
+            }
+        }
+        public bool IsCartaoCredito
+        {
+            get
+            {
+                return Id == (int)TipoFormaPagamentoEnum.CartaoCredito;
+            }
+        }
+        public bool IsDeposito
+        {
+            get
+            {
+                return Id == (int)TipoFormaPagamentoEnum.Deposito;
+            }
+        }
+        public bool IsNaoDefinido
+        {
+            get
+            {
+                return Id == (int)TipoFormaPagamentoEnum.NaoDefinido;
+            }
+        }
+    }
+}
